6df125567147b62eb5f81e10e61b189bdeaf9c37
João Pedro <jpdc2003@gmail.com>
2023-05-10 20:34:36 -0300

    Orientação de Objetos

---
+++ conta.py
@@ -0,0 +1,10 @@
+
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto... {}".format(self))
+        self.numero = 123
+        self.titulo = "João"
+        self.saldo = 55.0
+        self.limite = 1000.0
