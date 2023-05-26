09d212331a20d6900f4fa2159fd9968c09a0d69e
João Pedro <jpdc2003@gmail.com>
2023-05-26 19:10:27 -0300

    Orientação de Objetos v5 (final)

--- conta.py
+++ conta.py
@@ -8,6 +8,7 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        self.__codigo = "001"
 
     def extrato(self):
         print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
@@ -15,16 +16,25 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite disponivel".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
 
+    @property
     def get_titular(self):
         return self.__titular
 
@@ -35,3 +45,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
